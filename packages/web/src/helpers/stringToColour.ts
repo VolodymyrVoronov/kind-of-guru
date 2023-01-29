21220f2b45272d662933cbf8b9533ef4dd9d8c7b
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-01-29 18:23:15 +0100

    chore(web): added new helper function => convertStringToColor.

---
+++ packages/web/src/helpers/stringToColour.ts
@@ -0,0 +1,22 @@
+const convertStringToColor = (str: string): string => {
+  if (!str) {
+    throw new Error("No str provided");
+  }
+
+  let hash = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    // eslint-disable-next-line no-bitwise
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+  for (let i = 0; i < 3; i += 1) {
+    // eslint-disable-next-line no-bitwise
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.substr(-2);
+  }
+
+  return color;
+};
+
+export default convertStringToColor;
