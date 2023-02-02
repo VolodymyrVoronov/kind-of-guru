fe8dd4688ed652440387ad6e7c50fac213ae9e40
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-02-02 18:55:22 +0100

    chore(web): updated ProjectCard, displayed all information about porject.

--- packages/web/src/components/ProjectCard/ProjectCard.tsx
+++ packages/web/src/components/ProjectCard/ProjectCard.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import {
-  Avatar,
   Button,
   Card,
-  Collapse,
   Container,
   Grid,
   Spacer,
   Text,
   Tooltip,
 } from "@nextui-org/react";
+import { ImPaintFormat, ImMan, ImEnter, ImExit, ImInfo } from "react-icons/im";
+import { IoTrashSharp, IoPencilSharp } from "react-icons/io5";
 
 import IProject from "../../types/Project";
 
@@ -93,7 +93,224 @@ const ProjectCard = ({
           </Button>
         }
       />
-      T
+
+      <Grid.Container gap={0.5}>
+        <Grid xs={4} alignItems="baseline" direction="column">
+          <Text
+            h4
+            css={{
+              d: "flex",
+              alignItems: "baseline",
+              m: 0,
+            }}
+          >
+            Project: <Spacer x={0.5} /> <ImPaintFormat display="flex" />
+          </Text>
+          <Text
+            h3
+            css={{
+              m: 0,
+              color: "$blue600",
+            }}
+          >
+            {projectName}
+          </Text>
+        </Grid>
+
+        <Grid xs={4} direction="column">
+          <Text
+            h4
+            css={{
+              d: "flex",
+              alignItems: "baseline",
+              m: 0,
+            }}
+          >
+            Client: <Spacer x={0.5} /> <ImMan display="flex" />
+          </Text>
+          <Text
+            h3
+            css={{
+              m: 0,
+              color: "$blue600",
+            }}
+          >
+            {client}
+          </Text>
+        </Grid>
+        <Grid xs={4}>
+          <Grid.Container justify="space-between">
+            <Grid xs={5} direction="column">
+              <Text
+                h4
+                css={{
+                  d: "flex",
+                  alignItems: "baseline",
+                  m: 0,
+                }}
+              >
+                Start: <Spacer x={0.5} />
+                <Tooltip color="invert" content="Start date of the project">
+                  <ImEnter display="flex" />
+                </Tooltip>
+              </Text>
+              {start ? (
+                <Text
+                  h4
+                  css={{
+                    m: 0,
+                    color: "$blue600",
+                  }}
+                >
+                  {reverseDateString(start)}
+                </Text>
+              ) : (
+                <Text
+                  b
+                  css={{
+                    textGradient: "45deg, $yellow600 -20%, $red600 100%",
+                  }}
+                >
+                  unknown
+                </Text>
+              )}
+            </Grid>
+            <Grid xs={5} direction="column">
+              <Text
+                h4
+                css={{
+                  d: "flex",
+                  alignItems: "baseline",
+                  m: 0,
+                }}
+              >
+                End: <Spacer x={0.5} />
+                <Tooltip color="invert" content="End date of the project">
+                  <ImExit display="flex" />
+                </Tooltip>
+              </Text>
+              {end ? (
+                <Text
+                  h4
+                  css={{
+                    m: 0,
+                    color: "$blue600",
+                  }}
+                >
+                  {reverseDateString(end)}
+                </Text>
+              ) : (
+                <Text
+                  b
+                  css={{
+                    textGradient: "45deg, $yellow600 -20%, $red600 100%",
+                  }}
+                >
+                  unknown
+                </Text>
+              )}
+            </Grid>
+          </Grid.Container>
+        </Grid>
+      </Grid.Container>
+
+      <Spacer x={1} />
+
+      <Grid.Container gap={1}>
+        <Grid xs={7.5} direction="column">
+          <Text
+            h4
+            css={{
+              d: "flex",
+              alignItems: "center",
+              m: 0,
+            }}
+          >
+            Information: <Spacer x={0.5} />
+            <ImInfo display="flex" />
+          </Text>
+          {information ? (
+            <Text b>{information}</Text>
+          ) : (
+            <Text
+              b
+              css={{
+                textGradient: "45deg, $yellow600 -20%, $red600 100%",
+              }}
+            >
+              unknown
+            </Text>
+          )}
+        </Grid>
+
+        <Spacer x={1.5} />
+
+        <Grid xs={4} direction="row">
+          <Grid.Container justify="space-between">
+            <Grid xs={5} direction="row" alignItems="flex-start">
+              <Text
+                h4
+                css={{
+                  d: "flex",
+                  alignItems: "center",
+                  m: 0,
+                }}
+              >
+                Priority: <Spacer x={0.5} />
+                {getPriorityIcon(priority.toLowerCase())}
+              </Text>
+            </Grid>
+            <Grid xs={5} direction="row" alignItems="flex-start">
+              <Text
+                h4
+                css={{
+                  d: "flex",
+                  alignItems: "center",
+                  m: 0,
+                }}
+              >
+                Package: <Spacer x={0.5} />
+                {getPackageTypeIcon(packageType.toLowerCase())}
+              </Text>
+            </Grid>
+          </Grid.Container>
+        </Grid>
+      </Grid.Container>
+
+      <Spacer y={1} />
+
+      <Container
+        display="flex"
+        direction="row"
+        justify="flex-end"
+        css={{
+          width: "100%",
+          padding: "0",
+        }}
+      >
+        <Button
+          onPress={onDeleteCardButtonClick}
+          size="md"
+          ghost
+          rounded
+          color="error"
+          iconRight={<IoTrashSharp />}
+        >
+          Delete
+        </Button>
+
+        <Spacer x={1} />
+
+        <Button
+          onPress={onEditButtonClick}
+          size="md"
+          rounded
+          color="gradient"
+          iconRight={<IoPencilSharp />}
+        >
+          Edit
+        </Button>
+      </Container>
     </Card>
   );
 };
